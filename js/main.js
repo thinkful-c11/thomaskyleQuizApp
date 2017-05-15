3467d0efedbd33d9a6ca056355ed36aea18a0fd8
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-05-15 09:33:28 -0700

    finish adding random arrays

--- js/main.js
+++ js/main.js
@@ -1,28 +1,181 @@
 // appState
-const QUESTIONS = [
+const questions = [
   {
-    qstnID: 1
-    question: 'Question as a string.',
+    qstnID: 1,
+    question: 'Question as a string.' + this.qstnID,
     answers: {
       a: 'String of answer A',
       b: 'String of answer B',
       c: 'String of answer C',
       d: 'String of answer D',
+      e: 'String of answer E'
     },
-    crctAnsr: a,
+    crctAnsr: 'a'
+  },
+    {
+    qstnID: 2,
+    question: 'Question as a string.' +this.qstnID,
+    answers: {
+      a: 'String of answer A',
+      b: 'String of answer B',
+      c: 'String of answer C',
+      d: 'String of answer D',
+      e: 'String of answer E'
+    },
+    crctAnsr: 'a'
+  },
+    {
+    qstnID: 3,
+    question: 'Question as a string.' +this.qstnID,
+    answers: {
+      a: 'String of answer A',
+      b: 'String of answer B',
+      c: 'String of answer C',
+      d: 'String of answer D',
+      e: 'String of answer E'
+    },
+    crctAnsr: 'a'
+  },
+    {
+    qstnID: 4,
+    question: 'Question as a string.' +this.qstnID,
+    answers: {
+      a: 'String of answer A',
+      b: 'String of answer B',
+      c: 'String of answer C',
+      d: 'String of answer D',
+      e: 'String of answer E'
+    },
+    crctAnsr: 'a'
+  },
+    {
+    qstnID: 5,
+    question: 'Question as a string.' +this.qstnID,
+    answers: {
+      a: 'String of answer A',
+      b: 'String of answer B',
+      c: 'String of answer C',
+      d: 'String of answer D',
+      e: 'String of answer E'
+    },
+    crctAnsr: 'a'
+  },
+    {
+    qstnID: 6,
+    question: 'Question as a string.' +this.qstnID,
+    answers: {
+      a: 'String of answer A',
+      b: 'String of answer B',
+      c: 'String of answer C',
+      d: 'String of answer D',
+      e: 'String of answer E'
+    },
+    crctAnsr: 'a'
+  },
+    {
+    qstnID: 7,
+    question: 'Question as a string.' +this.qstnID,
+    answers: {
+      a: 'String of answer A',
+      b: 'String of answer B',
+      c: 'String of answer C',
+      d: 'String of answer D',
+      e: 'String of answer E'
+    },
+    crctAnsr: 'a'
+  },
+    {
+    qstnID: 8,
+    question: 'Question as a string.' +this.qstnID,
+    answers: {
+      a: 'String of answer A',
+      b: 'String of answer B',
+      c: 'String of answer C',
+      d: 'String of answer D',
+      e: 'String of answer E'
+    },
+    crctAnsr: 'a'
+  },
+    {
+    qstnID: 9,
+    question: 'Question as a string.' +this.qstnID,
+    answers: {
+      a: 'String of answer A',
+      b: 'String of answer B',
+      c: 'String of answer C',
+      d: 'String of answer D',
+      e: 'String of answer E'
+    },
+    crctAnsr: 'a'
+  },
+    {
+    qstnID: 10,
+    question: 'Question as a string.' +this.qstnID,
+    answers: {
+      a: 'String of answer A',
+      b: 'String of answer B',
+      c: 'String of answer C',
+      d: 'String of answer D',
+      e: 'String of answer E'
+    },
+    crctAnsr: 'a'
   },
 ];
 const appState = {
-  crntPosition: 0,
-  crntCrct: 0,
-  crntIncrct: 0,
+  curPos: 0,
+  curCrct: 0,
+  crntIncrct: () => curPos - curCrct,
   userAns: {
     // 1: null,
   },
+  question:[],
 };
 
 // State manipulation functions
+//increment Question position
+function incQuestionPos(state){
+  return state.curPos++;
+}
+
+//increment Correct Answer Counter
+function incCorrectAns(state){
+  return state.curCrct++;
+}
+
+//gives a random id in questions arr
+function randQuestion(state){
+  return Math.floor(Math.random()*questions.length + 1);
+}
+
+//no dupicate question
+function noDup(state, randID){
+  if(!(state.question.find(el=>el.qstnID===randID))){
+    return true;
+  }
+  return false;
+}
+
+//add to question array
+function addRandQuestion(state){
+  let randID;
+  while(true){
+    if(state.question.length === 5){
+      break;
+    }else{
+      randID = randQuestion(state);
+      if(noDup(state,randID)){
+        state.question.push(questions.find(el => el.qstnID === randID));
+
+      }
+    }
+  }
+  return state.question;
+}
+console.log(addRandQuestion(appState));
+console.log(`state question array: ${appState.question[0].question} hii` );
 
 // Render function(s)
+function renderNewQuestion(){
 
+}
 // Event handlers
