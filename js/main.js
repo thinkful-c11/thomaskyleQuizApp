aa97440007d4afbd010ac23de53a4f1bad23c0fe
tckastanek <tckastanek@gmail.com>
2017-05-15 11:18:56 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// appState
+const QUESTIONS = [
+  {
+    qstnID: 1
+    question: 'Question as a string.',
+    answers: {
+      a: 'String of answer A',
+      b: 'String of answer B',
+      c: 'String of answer C',
+      d: 'String of answer D',
+    },
+    crctAnsr: a,
+  },
+];
+const appState = {
+  crntPosition: 0,
+  crntCrct: 0,
+  crntIncrct: 0,
+  userAns: {
+    // 1: null,
+  },
+};
+
+// State manipulation functions
+
+// Render function(s)
+
+// Event handlers
